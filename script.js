e28a034d85d2fa9f193c38862b42edd7442af8ea
fuyukicy <balictarnajah@gmail.com>
2025-08-03 10:49:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.start-btn').addEventListener('click', function() {
+  document.querySelector('.intro-screen').style.display = 'none';
+  document.getElementById('message').style.display = 'block';
+});
