f414b1f9b6f83f6d2404dd61f7f8d85bea7fae2d
fuyukicy <balictarnajah@gmail.com>
2025-08-03 10:54:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.querySelector('.start-btn').addEventListener('click', function() {
-  document.querySelector('.intro-screen').style.display = 'none';
-  document.getElementById('message').style.display = 'block';
-});
+window.onload = function() {
+  document.querySelector('.start-btn').addEventListener('click', function() {
+    document.querySelector('.intro-screen').style.display = 'none';
+    document.getElementById('message').style.display = 'block';
+
+    const fortune = document.querySelector('.fortune-text');
+    fortune.classList.add('reveal');
+  });
+};
